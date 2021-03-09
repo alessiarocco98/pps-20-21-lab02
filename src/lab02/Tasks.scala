1b2731b8b1c75321c642fbb70159a5c63fdee219
Alessia Rocco <alessia.rocco@studio.unibo.it>
2021-03-09 19:03:28 +0100

    exercise 6 part 3 and its test done

--- src/lab02/Tasks.scala
+++ src/lab02/Tasks.scala
@@ -36,4 +36,26 @@ object Tasks {
   def compose(f: Int => Int, g: Int => Int): Int => Int = x => f(g(x))
   // generic version
   def composeG[A] (f: A => A, g: A => A): A => A = x => f(g(x))
+
+  /*--- Tasks - part 3 (recursion) ---*/
+
+  /* 6. */
+  // version non tail recursion
+  def fibonacci(n:Int): Int = n match {
+    case 0 => 0
+    case 1 => 1
+    case n if n > 1 => fibonacci(n - 1) + fibonacci(n - 2)
+    case n if n < 0 => -1 // per evitare il loop infinito in caso si inserisca un numero negativo
+  }
+  // tail rec version:
+  def fib(n: Int): Int = {
+     @annotation.tailrec
+    def _fib(n: Int, a: Int, b:Int): Int = n match {
+       case 0 => a
+       case 1 => b
+       case _ => _fib(n - 1, b, b + a)
+     }
+    _fib(n, 0, 1)
+  }
+
 }
--- test/lab02/TasksTest.scala
+++ test/lab02/TasksTest.scala
@@ -64,4 +64,19 @@ class TasksTest {
     assertEquals("ciao1ciao", composeG[String](_+"ciao", _+"1")("ciao"))
   }
 
+  @Test def testFib() {
+    assertEquals(0, fibonacci(0))
+    assertEquals(1,fibonacci(1))
+    assertEquals(1,fibonacci(2))
+    assertEquals(2,fibonacci(3))
+    assertEquals(3,fibonacci(4))
+    assertEquals(-1,fibonacci(-5))
+
+    // tail recursion version:
+    assertEquals(0, fib(0))
+    assertEquals(1,fib(1))
+    assertEquals(1,fib(2))
+    assertEquals(2,fib(3))
+    assertEquals(3,fib(4))
+  }
 }
